849e66f3b93ab087ecdbd988d852bf10cd21cf2a
David Tolnay <dtolnay@gmail.com>
2024-03-25 21:53:41 -0700

    Release 0.1.5

--- src/lib.rs
+++ src/lib.rs
@@ -57,7 +57,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/rustflags/0.1.4")]
+#![doc(html_root_url = "https://docs.rs/rustflags/0.1.5")]
 #![allow(
     clippy::cast_lossless,
     clippy::derive_partial_eq_without_eq,
